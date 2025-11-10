a47a46a4cec68b2394af1a18c1ca8d2ec4914a60
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-11-10 23:13:52 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Login_page_01());
+}
+
+class Login_page_01 extends StatelessWidget {
+  const Login_page_01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
